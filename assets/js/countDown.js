ee1267f7e7db5634244eee367ac0fd0286dbeb18
Ricardo <50000459+RicardoValls@users.noreply.github.com>
2024-03-20 00:26:13 +0100

    Update countDown.js

--- assets/js/countDown.js
+++ assets/js/countDown.js
@@ -1,1 +1,30 @@
+// Set the date we're counting down to
+var releaseDate = new Date("Apr 1, 2024 00:00:00").getTime();
 
+// Update the count down every 1 second
+var refresh = setInterval(function baseCountDown() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let timeLeft = releaseDate - now;
+
+  //Check if the date has expired
+  if (timeLeft < 0) {
+    clearInterval(refresh);
+    document.getElementById("countMessage").innerHTML = "UHOH, Something went wrong!";
+  }  else {
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        
+    // Output the result in an element with id="countDown"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+    }
+}, 1000);
